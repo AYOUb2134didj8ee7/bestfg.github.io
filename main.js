b844ce3351f82232e99a61f21ed821d5559cafff
Reality <dgfhigufufjghhhfuftfh@gmail.com>
2026-01-29 15:44:49 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+const welcome = document.getElementById("welcome");
+const teamsScreen = document.getElementById("teams");
+const joinBtn = document.getElementById("joinBtn");
+const teamsList = document.getElementById("teamsList");
+const startBtn = document.getElementById("startBtn");
+
+let selectedTeam = null;
+
+// Join
+joinBtn.onclick = () => {
+    welcome.classList.remove("active");
+    teamsScreen.classList.add("active");
+    loadTeams();
+};
+
+// Load teams
+function loadTeams() {
+    teamsList.innerHTML = "";
+    teams.forEach(team => {
+        const div = document.createElement("div");
+        div.className = "team";
+        div.innerHTML = `
+            <img src="${team.icon}">
+            <strong>${team.name}</strong>
+        `;
+
+        div.onclick = () => selectTeam(team, div);
+        teamsList.appendChild(div);
+    });
+}
+
+// Select team
+function selectTeam(team, element) {
+    document.querySelectorAll(".team").forEach(t => t.classList.remove("selected"));
+    element.classList.add("selected");
+    selectedTeam = team;
+    startBtn.disabled = false;
+}
+
+// Start game
+startBtn.onclick = () => {
+    localStorage.setItem("selectedTeam", JSON.stringify(selectedTeam));
+    window.location.href = "game.html";
+};
---
+++ team.js
@@ -0,0 +1,26 @@
+const teams = [
+    {
+        id: "rm",
+        name: "Real Madrid",
+        icon: "assets/icons/realmadrid.png",
+        players: ["Vinicius", "Bellingham", "Rodrygo", "Modric"]
+    },
+    {
+        id: "barca",
+        name: "FC Barcelona",
+        icon: "assets/icons/barcelona.png",
+        players: ["Pedri", "Gavi", "Lewandowski", "Yamal"]
+    },
+    {
+        id: "city",
+        name: "Man City",
+        icon: "assets/icons/mancity.png",
+        players: ["Haaland", "De Bruyne", "Foden", "Silva"]
+    },
+    {
+        id: "psg",
+        name: "PSG",
+        icon: "assets/icons/psg.png",
+        players: ["Mbappé", "Dembele", "Vitinha", "Hakimi"]
+    }
+];
